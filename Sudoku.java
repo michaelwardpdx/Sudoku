febbfb8630f62b5c628dcae70474e914a76c6e3a
mike <michaelward@sprintmail.com>
2017-02-15 21:58:03 -0800

    Added Sudoku.java

---
+++ Sudoku.java
@@ -0,0 +1,25 @@
+import javafx.application.*;
+import javafx.scene.*;
+import javafx.stage.*;
+import javafx.scene.layout.*;
+import javafx.scene.control.*;
+public class Sudoku extends Application {
+    public static void main(String[] args) {
+        System.out.println("Launching JavaFX application.");
+        launch(args);
+    }
+    public void init() {
+        System.out.println("Inside the init() method.");
+    }
+    public void start(Stage myStage) {
+        System.out.println("Inside the start() method.");
+        myStage.setTitle("Sudoku Solver");
+        FlowPane rootNode = new FlowPane();
+        Scene myScene = new Scene(rootNode, 300, 200);
+        myStage.setScene(myScene);
+        myStage.show();
+    }
+    public void stop() {
+        System.out.println("Inside the stop() method.");
+    }
+}
